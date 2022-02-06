1e45a8037cd36da4b0007fb1ef53f880b571d639
Shalvi28 <g1992.shalvi@gmail.com>
2022-02-06 15:53:51 +0530

    Updates

---
+++ precache-manifest.d98a58c78d5640bb32ce41d0d3021c54.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "990a73dcdb0555604cbb",
+    "url": "/react_application_form/static/css/main.d4f0c139.chunk.css"
+  },
+  {
+    "revision": "990a73dcdb0555604cbb",
+    "url": "/react_application_form/static/js/main.d39042c0.chunk.js"
+  },
+  {
+    "revision": "19ecaf4b3ecf055a2203",
+    "url": "/react_application_form/static/js/runtime~main.e3d86749.js"
+  },
+  {
+    "revision": "a5e9d36056a6ac6e590a",
+    "url": "/react_application_form/static/js/2.f4f735b6.chunk.js"
+  },
+  {
+    "revision": "bbf0ef291828a0044611bf37ae72c3c3",
+    "url": "/react_application_form/index.html"
+  }
+];
